e057543cca377f2644933b8ea4f3c137cda72545
xjpa <xjpa@proton.me>
2023-12-23 12:13:46 -0500

    window

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import turtle
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+screen = turtle.Screen()
+screen.title("PyPong")
+screen.bgcolor("Gray")
+screen.setup(width=800, height=800)
+screen.tracer(0)
+while(1):
+    screen.update()
